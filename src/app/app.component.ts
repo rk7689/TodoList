cf111ae0866875eda05dc57bcfaa0237a593b756
Kenjale <rohan.kenjale@siemens.com>
2020-07-07 00:10:21 +0530

    feat: todo list app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  name:string = 'Rohan';
+
+  constructor() {
+    this.changeName('Anuja Mental');
+  }
+
+  changeName(name:string):void{
+    this.name = name;
+  }
 }
